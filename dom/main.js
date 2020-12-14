c3d8a892eeadef6cd7420e222778e0153a846ed1
GonzaloMonteodorisio <56704895+GonzaloMonteodorisio@users.noreply.github.com>
2020-12-14 15:42:06 -0300

    second commit

--- dom/main.js
+++ dom/main.js
@@ -1,7 +1,6 @@
 const title = document.getElementById('title');
-console.log(title);
 
-title.textContent = "DOM - Accediendo a nodos";
+//title.textContent = "DOM - Accediendo a nodos";
 
 const paragraph = document.querySelector('.paragraph:nth-child(1)');
 
@@ -20,10 +19,35 @@ paragraphsSpread.map(p => p.style.color = 'green');
 
 //convierte a paragraphs en un array 8funciona en todos los navegadores)
 const paragraphsArray = Array.from(document.querySelectorAll('.paragraph'));
-paragraphsSpread.map(p => p.style.fontSize = '32px');
+paragraphsSpread.map(p => p.style.fontSize = '25px');
 //no es la forma correcta cambiar el CSS de esta forma, en línea
 paragraphs[0].style.fontWeight = 'bold';
 
-
 console.log(paragraphs);
 
+////////////////////////////////////////////////////////////////////////
+//Modificar atributos y clases
+
+const name = document.getElementById('name');
+const date = document.getElementById('date');
+console.log(name.getAttribute('type'));
+
+date.setAttribute('type', 'date');
+console.log(date.getAttribute('type'));
+
+title.classList.add('main-title', 'other-title');
+title.classList.remove('main-title');
+
+if(title.classList.contains('title')) {
+	console.log('Contiene la clase "title"');
+} else {
+	console.log('No contiene la clase "title"');
+}
+
+title.classList.replace('title', 'main-title');
+console.log(name);
+console.log(title);
+console.log(date);
+console.log(title.innerHTML);
+console.log(title.textContent);
+console.log(name.value);
